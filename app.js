93f221d6e521b00671fd349f66f691410e400e6d
mrsupon <supon.sup@gmail.com>
2023-09-27 22:49:41 +0700

    add vercel.json

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const i = ["Buy Food", "Cook Food", "Eat Food"];
 const db = "todoList";
 async function main() {
   await mongoose.connect('mongodb+srv://admin:Ss12345678@cluster0.twlafbw.mongodb.net/'+db);
+  //await mongoose.connect('mongodb://127.0.0.1:27017/'+db);
 }
 main().catch(err => console.log(err));
 
